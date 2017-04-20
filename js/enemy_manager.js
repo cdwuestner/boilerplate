c92534cc95adfcbdf05cbcd40fb9a41d64c381f3
Chris Wuestner <cdwuestner@gmail.com>
2017-04-20 17:21:51 -0500

    Enemy movement completed

--- js/enemy_manager.js
+++ js/enemy_manager.js
@@ -16,5 +16,39 @@ game.EnemyManager = me.Container.extend({
           }
       }
       this.updateChildBounds();
+   },
+   
+   onActivateEvent : function () {
+	   var _this = this;
+	   this.timer = me.timer.setInterval(function () {
+		   var bounds = _this.childBounds;
+		   
+		   if ((_this.vel > 0 && (bounds.right + _this.vel) >= me.game.viewport.width) || 
+		   (_this.vel < 0 && (bounds.left + _this.vel) <= 0)) {
+			   _this.vel *= -1;
+			   _this.pos.y += 16;
+			   if (_this.vel > 0) {
+				   _this.vel += 5;
+			   } else {
+				   _this.vel -= 5;
+			   }
+		   } else {
+			   _this.pos.x += _this.vel;
+		   }
+	   }, 1000);
+   },
+   
+   onDeactivateEvent : function () {
+	   me.timer.clearInterval(this.timer);
+   },
+   
+   removeChildNow : function (child) {
+	   this._super(me.Container, "removeChildNow", [child]);
+	   this.updateChildBounds();
+   },
+   
+   update : function (time) {
+	   this._super(me.Container, "update", [time]);
+	   this.updateChildBounds();
    }
 });
